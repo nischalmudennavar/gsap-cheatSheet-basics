0f8884b424112ed15c9725d5c94b54633cb67835
nischalmudennavar <nischalmudennavar@gmail.com>
2021-03-30 18:15:05 +0530

    stagger

--- js/main.js
+++ js/main.js
@@ -17,6 +17,16 @@ document.querySelector('#effect-2').onclick = function(){
     })
 }
 
+document.querySelector('#effect-3').onclick = function(){
+
+    gsap.to('.subject-translatex',{
+        x:100,
+        duration:0.5,
+        stagger:0.4
+        
+    })
+}
+
 document.querySelectorAll('.language-javascript').forEach(block => {
     // then highlight each
     hljs.highlightBlock(block);
