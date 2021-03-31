3435b9c58f92cf3ac1f912158f45b15de93f55f2
nischalmudennavar <nischalmudennavar@gmail.com>
2021-03-31 12:39:20 +0530

    Rotate added

--- js/main.js
+++ js/main.js
@@ -2,25 +2,26 @@ var copyTextareaBtn = document.querySelector('.copy-btn');
 
 
 
+
 function myFunction() {
     /* Get the text field */
     var copyText = document.getElementById("cdn");
-  
+
     /* Select the text field */
     copyText.select();
     copyText.setSelectionRange(0, 99999); /* For mobile devices */
-  
+
     /* Copy the text inside the text field */
     document.execCommand("copy");
 
 
-  
+
     /* Alert the copied text */
-    alert("cdn copied 😉🕺" );
+    alert(" CDN copied 😉🕺 " );
 
     // var tooltip = document.getElementById("myTooltip");
     //    tooltip.innerHTML = "Copied cdn ";
-  }
+}
 
 
 document.querySelector('#effect-1').onclick = function(){
@@ -74,12 +75,36 @@ document.querySelector('#effect-3-2').onclick = function(){
     gsap.to('.subject-translatexs',{
         x:-100,
         duration:0.5,
-        stagger:0.4
+       
         
     })
 }
 
+document.querySelector('#effect-4').onclick = function(){
+
+    gsap.to('.subject-rotate',{
+        x:100,
+        duration:0.5,
+        rotate:90,
+ 
+       
+        
+    })
+}
+document.querySelector('#effect-4-2').onclick = function(){
+
+    gsap.to('.subject-rotate ',{
+        x:-100,
+        duration:0.5,
+        rotate:-90
+        
+        
+    })
+}
+let colors = ['red','blue','green','yellow','orange']
+
 document.querySelectorAll('.language-javascript').forEach(block => {
     // then highlight each
     hljs.highlightBlock(block);
+    // block.style.color =colors[Math.random()*3];
   });
